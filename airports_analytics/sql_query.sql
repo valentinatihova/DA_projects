d9445d47d75672759c68f2845da04d3bef2a42a9
Valentina <62104692+valentinatihova@users.noreply.github.com>
2020-07-08 18:36:23 +0300

    Update sql_query.sql

--- airports_analytics/sql_query.sql
+++ airports_analytics/sql_query.sql
@@ -1,32 +1,34 @@
-/* Изучим таблицу airports и выведем список городов (city), в которых есть аэропорты */
+/* 1. Изучим таблицу airports и выведем список городов (city), в которых есть аэропорты */
 
 SELECT DISTINCT(city)
 FROM airports
 
-/* Изучим таблицу flights и подсчитаем количество вылетов (flight_id) из каждого аэропорта вылета (departure_airport). Назовем переменную cnt_flights и выведем её вместе со столбцом departure_airport. Результат отсортируем в порядке убывания количества вылетов.*/
+/* Изучим таблицу flights и подсчитаем количество вылетов (flight_id) из каждого аэропорта вылета (departure_airport). 
+Назовем переменную cnt_flights и выведем её вместе со столбцом departure_airport. 
+Результат отсортируем в порядке убывания количества вылетов.*/
 
 SELECT departure_airport, COUNT(flight_id) AS cnt_flights
 FROM flights
 GROUP BY departure_airport
 ORDER BY cnt_flights DESC
 
-/* Найдем количество рейсов на каждой модели самолёта с вылетом в сентябре 2018 года (flights_amount).*/
+/* 2. Найдем количество рейсов на каждой модели самолёта с вылетом в сентябре 2018 года (flights_amount).*/
 
 SELECT model, COUNT(flights.flight_id) AS flights_amount
 FROM aircrafts
-INNER JOIN flights on flights.aircraft_code = aircrafts.aircraft_code
-WHERE EXTRACT(month from flights.DEPARTURE_TIME) = 9 
+INNER JOIN flights ON flights.aircraft_code = aircrafts.aircraft_code
+WHERE EXTRACT(month FROM flights.DEPARTURE_TIME) = 9 
 GROUP BY model
 
-/* Найдем количество рейсов по всем моделям самолетов Boeing и Airbus в сентябре (flights_amount).*/
+/* 3. Найдем количество рейсов по всем моделям самолетов Boeing и Airbus в сентябре (flights_amount).*/
 
 SELECT COUNT(flights.flight_id) AS flights_amount
 FROM aircrafts
-INNER JOIN flights on flights.aircraft_code = aircrafts.aircraft_code
-WHERE EXTRACT(month from flights.DEPARTURE_TIME) = 9 
+INNER JOIN flights ON flights.aircraft_code = aircrafts.aircraft_code
+WHERE EXTRACT(month FROM flights.DEPARTURE_TIME) = 9 
         AND (aircrafts.model LIKE 'Airbus%' OR aircrafts.model LIKE 'Boeing%')
 
-/* Найдем среднее количество прибывающих рейсов в день для каждого города за август 2018 года (average_flights). */
+/* 4. Найдем среднее количество прибывающих рейсов в день для каждого города за август 2018 года (average_flights). */
 
 SELECT sql1.city, AVG(sql1.avg_flights) AS average_flights
 FROM
@@ -39,14 +41,18 @@ GROUP BY airports.city,  EXTRACT(day FROM flights.arrival_TIME)) AS sql1
 
 GROUP BY sql1.city
 
-/* Установим фестивали, которые проходили с 23 июля по 30 сентября 2018 года в Москве, и номер недели, в которую они проходили. Выведите название фестиваля festival_name и номер недели festival_week. */
+/* 5. Установим фестивали, которые проходили с 23 июля по 30 сентября 2018 года в Москве, 
+   и номер недели, в которую они проходили. Выведите название фестиваля festival_name и номер недели festival_week. */
 
 SELECT festival_name, EXTRACT (WEEK FROM festival_date) AS festival_week
 FROM festivals
 WHERE (festival_date BETWEEN '2018-07-23' AND '2018-09-30') AND festival_city = 'Москва'
 ORDER BY 2
 
-/* Для каждой недели с 23 июля по 30 сентября 2018 года узнаем количество билетов, купленных на рейсы в Москву (номер недели week_number и количество билетов ticket_amount). Получим таблицу, в которой будет информация о количестве купленных за неделю билетов; отметка, проходил ли в эту неделю фестиваль; название фестиваля festival_name и номер недели week_number. */
+/* 6. Для каждой недели с 23 июля по 30 сентября 2018 года узнаем количество билетов, купленных на рейсы в Москву 
+   (номер недели week_number и количество билетов ticket_amount). 
+    Получим таблицу, в которой будет информация о количестве купленных за неделю билетов; отметка, проходил ли в эту неделю фестиваль; 
+    название фестиваля festival_name и номер недели week_number. */
 
 SELECT 
     SBQ.week_number,
@@ -68,4 +74,5 @@ LEFT JOIN
 ON 
     SBQ.week_number = SBQ1.festival_week 
 
-/* В результате получена небольшая таблица: по ней видно, что спрос во время фестивалей практически не меняется. По этим данным нельзя проверить гипотезу о связи музыкальных фестивалей и спроса на авиабилеты (критерий Стьюдента)*/
+/* В результате получена небольшая таблица: по ней видно, что спрос во время фестивалей практически не меняется. 
+   По этим данным нельзя проверить гипотезу о связи музыкальных фестивалей и спроса на авиабилеты (критерий Стьюдента)*/
